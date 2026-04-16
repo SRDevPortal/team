9d2b5ecd8fefbfda039d0f9902e8ccc29efcc1eb
SRDevPortal <webdevelopersriaas@gmail.com>
2026-04-16 17:34:21 +0530

    Update runner logic

--- team/setup/runner.py
+++ team/setup/runner.py
@@ -1,9 +1,5 @@
 from .custom_fields import apply_custom_fields
-from .doctypes import ensure_team_doctypes
-from .utils import ensure_module_def
 
 
 def setup_all():
-	ensure_module_def()
-	ensure_team_doctypes()
 	apply_custom_fields()
